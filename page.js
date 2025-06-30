e51ab21f93320057d340bb4d86f387a6a8a61fc7
CerebroOra <looptimee@gmail.com>
2025-06-30 14:58:45 -0300

    Create page.js

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{ fontSize: '2.5rem' }}>Mandala Viva</h1>
+      <p>Versão funcional com App Router pronta para o Vercel.</p>
+    </main>
+  )
+}
